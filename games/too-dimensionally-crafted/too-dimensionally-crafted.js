d572f6a3e9eeb6ac9a5c3cfac925a8c085836824
DeveloperYouTube <11001dev.alex10100@gmail.com>
2025-03-24 09:25:39 -0700

    asdasdasd

--- games/too-dimensionally-crafted/too-dimensionally-crafted.js
+++ games/too-dimensionally-crafted/too-dimensionally-crafted.js
@@ -245,7 +245,7 @@ function load_blocks(x, y) {
             blocks[block_key] = 0; // Grass at noiseFloor
         } else if (y < noiseFloor) {
             blocks[block_key] = 3; // Air above noiseFloor
-        } else if (y > noiseFloor && y >= noiseFloor + 3 + Math.random()) {
+        } else if (y > noiseFloor && y >= noiseFloor - 3 + Math.random()) {
             blocks[block_key] = 4; // Block 4 at random distance below noiseFloor
         } else {
             blocks[block_key] = 1; // Dirt below noiseFloor (after block 4)
