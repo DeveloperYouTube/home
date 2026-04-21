eb77df6c0df751e22c50918fcb9b27a6c1afd844
DeveloperYouTube <11001dev.alex10100@gmail.com>
2026-04-21 13:35:44 -0700

    sdfg

--- games/too-dimensionally-crafted/local/1P/play/too-dimensionally-crafted.js
+++ games/too-dimensionally-crafted/local/1P/play/too-dimensionally-crafted.js
@@ -1,6 +1,13 @@
 import {utils} from '../../../../../utilities.js';
 //variables 
-const worldINIT = JSON.parse(localStorage.getItem('2DCsinglePworldJSON'));
+const savedData = localStorage.getItem('2DCsinglePworldJSON');
+
+let worldINIT;
+
+if (savedData !== null) {
+    // Inside this block, the editor knows for sure savedData is a string
+    worldINIT = JSON.parse(savedData);
+}
 const world_name = worldINIT.name;
 const game_mode = worldINIT.game_mode;
 const difficulty = worldINIT.difficulty;
@@ -12,3 +19,15 @@ let blocks = worldINIT.blocks;
 let playerHP = worldINIT.HP;
 let inventory = worldINIT.inventory;
 let entities = worldINIT.entities;
+function loadblock (/** @type {number} */x,/** @type {number} */y) {
+    const noiseValue = utils.perlin.noise(x, seed) * 10; // Adjust multiplier as needed
+    const noiseFloor = Math.round(noiseValue);
+    if (noiseFloor==y){
+        return 1;
+    } else if (noiseFloor >= y) {
+        return 2
+    } else {return 0}
+}
+function load_blocks (/** @type {number} */x,/** @type {number} */y,/** @type {number} */X,/** @type {number} */Y) {
+    
+}
