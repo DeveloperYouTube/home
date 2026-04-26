3b0a06794833dc9bdc490a398d5a07d61ddae0a5
DeveloperYouTube <11001dev.alex10100@gmail.com>
2026-04-26 11:50:57 -0700

    hjk

--- games/too-dimensionally-crafted/local/1P/play/too-dimensionally-crafted.js
+++ games/too-dimensionally-crafted/local/1P/play/too-dimensionally-crafted.js
@@ -18,8 +18,13 @@ let blocks = worldINIT.blocks;
 let playerHP = worldINIT.HP;
 let inventory = worldINIT.inventory;
 let entities = worldINIT.entities;
+const mountain = Number(worldINIT.m);
+const sea= Number(worldINIT.o);
+const smoothness = Number(worldINIT.s);
+const height = mountain-sea
+
 function loadblock (/** @type {number} */x,/** @type {number} */y) {
-    const noiseValue = utils.perlin.noise(x, seed) * 10; // Adjust multiplier as needed
+    const noiseValue = utils.perlin.noise(x/smoothness, seed) * height + sea;
     const noiseFloor = Math.round(noiseValue);
     if (noiseFloor==y){
         return 1;
@@ -27,6 +32,13 @@ function loadblock (/** @type {number} */x,/** @type {number} */y) {
         return 2
     } else {return 0}
 }
+function load_block (/** @type {number} */x,/** @type {number} */y) {
+    if (!blocks[x+","+y]){blocks[x+","+y] = loadblock(x,y);}
+}
 function load_blocks (/** @type {number} */x,/** @type {number} */y,/** @type {number} */X,/** @type {number} */Y) {
-    
+    for (let i = x; i <= X; i++) {
+        for (let j=y; j<=Y; j++){
+            load_block(i,j);
+        }
+    }
 }
