f72e58cac30480728a1fa9c17372c1af1fa024ec
DeveloperYouTube <11001dev.alex10100@gmail.com>
2026-06-09 17:26:17 -0700

    sdfg

--- games/too-dimensionally-crafted/local/1P/play/too-dimensionally-crafted.js
+++ games/too-dimensionally-crafted/local/1P/play/too-dimensionally-crafted.js
@@ -73,6 +73,21 @@ const pause_screen = document.getElementById('pause_screen');
 pause_screen.style.display = "none";
 const textureCache = {};
 const blockSize = 32;
+// Velocities (Current speed in blocks per second)
+let vx = 0;
+let vy = 0;
+
+// Accelerations (Rate of speed change per second)
+let ax = 0;
+let ay = 0;
+
+// Minimum Speed Caps (Prevents falling or moving left too fast)
+const minvx = -8;
+const minvy = -24; // High terminal velocity downward for falling
+
+// Maximum Speed Caps (Prevents flying or moving right too fast)
+const maxvx = 8;
+const maxvy = 8;
 
 function getTexture(path) {
     // If we've already loaded this image, just return it
@@ -171,8 +186,57 @@ function draw() {
     ctx.fillStyle = "#008080";
     ctx.fillRect(centerX - 16, centerY - 32, 32, 64);
 }
+let crtl = false;
+// --- 'A' KEY (Move Left) ---
+window.addEventListener('keydown', (e) => {
+    if ((e.key !== 'a' && e.key !== 'A') || e.repeat) return;
+    ax = -10;
+    minvx = crtl ? -5.612 : -4.317;
+});
+window.addEventListener('keyup', (e) => {
+    if (e.key !== 'a' && e.key !== 'A') return;
+    maxvx = 0;
+    ax = 10;
+});
+
+// --- 'D' KEY (Move Right) ---
+window.addEventListener('keydown', (e) => {
+    if ((e.key !== 'd' && e.key !== 'D') || e.repeat) return;
+    ax = 10;
+    maxvx = crtl ? 5.612 : 4.317; 
+});
+window.addEventListener('keyup', (e) => {
+    if (e.key !== 'd' && e.key !== 'D') return;
+    ax = -10;
+    minvx = 0; 
+});
+
+window.addEventListener('keydown', (e) => {
+    // Check if the key being pressed is Control OR Command
+    if (e.key !== 'Control' && e.key !== 'Meta') return;
+    if (e.repeat) return;
+    crtl = true
+    minvx = minvx * (5.612/4.317);
+    maxvx = maxvx * (5.612/4.317);
+});
+
+window.addEventListener('keyup', (e) => {
+    if (e.key !== 'Control' && e.key !== 'Meta') return;
+    crtl = false
+    minvx = minvx * (4.317/5.612);
+    maxvx = maxvx * (4.317/5.612);
+});
 function update() {
     load_blocks(Math.round(playerX) - Math.ceil(canvas.width / 64) - 2, Math.round(playerY) - Math.ceil(canvas.height / 64) - 2, Math.round(playerX) + Math.ceil(canvas.width / 64) + 2, Math.round(playerY) + Math.ceil(canvas.height / 64) + 2);
+    vx += ax * dt;
+    vy += ay * dt;
+    // Clamp Horizontal Velocity
+    vx = Math.max(minvx, Math.min(vx, maxvx));
+
+    // Clamp Vertical Velocity
+    vy = Math.max(minvy, Math.min(vy, maxvy));
+    playerX += vx * dt;
+    playerY += vy * dt;
 }
 let lastTime = 0;
 function gameLoop() {
