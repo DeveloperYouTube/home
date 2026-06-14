56a5e9e771e1baa608e14ac9979c1c0ddd1d7c0c
DeveloperYouTube <11001dev.alex10100@gmail.com>
2026-06-14 16:27:24 -0700

    dfg

--- games/too-dimensionally-crafted/local/1P/play/too-dimensionally-crafted.js
+++ games/too-dimensionally-crafted/local/1P/play/too-dimensionally-crafted.js
@@ -1,6 +1,6 @@
 //imports
 import {utils} from '../../../../../utilities.js';
-
+console.log('coll v2');
 //varibles
 //const(can't change (e.g. HTML elements and objects))
 const world_dataINIT = JSON.parse(localStorage.getItem('2DCsinglePworldJSON'))
